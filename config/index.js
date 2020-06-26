b9e77cd3993ffaae10abee0add1f8437aeb174cb
Thanh Hai <hthai2201@gmail.com>
2020-06-26 10:54:44 +0700

    node env

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ const defaultConfig = require("./default.json");
 const node_env = process.env.NODE_ENV || "development";
 module.exports = {
   ...defaultConfig.common,
-  ...defaultConfig["development"],
+  ...defaultConfig[node_env],
 };
